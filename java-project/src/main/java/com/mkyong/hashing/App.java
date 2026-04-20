cccd2e926f5e03fdfee1aee8ac6bfe7e186ddfaf
subash-7483 <avsubash76@gmail.com>
2026-04-20 21:59:11 +0530

    Update App.java

--- java-project/src/main/java/com/mkyong/hashing/App.java
+++ java-project/src/main/java/com/mkyong/hashing/App.java
@@ -11,7 +11,7 @@ public class App {
             System.exit(0);
         }
         System.out.println(sha256hex(args[0]));
-
+        System.out.println("hello from subash");
     }
 
     public static String sha256hex(String input) {
